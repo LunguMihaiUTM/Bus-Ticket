94125130c48f86ee58d6274e5e91440e6e65f418
Lungu Mihai <lungu2003mihai1@mail.ru>
2025-02-08 01:06:34 +0200

    Add the TripController

---
+++ src/main/java/com/prod/busticket/controller/TripController.java
@@ -0,0 +1,22 @@
+package com.prod.busticket.controller;
+
+import com.prod.busticket.dto.TripDTO;
+import com.prod.busticket.service.TripService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/trip")
+public class TripController {
+
+    private final TripService tripService;
+
+    @GetMapping("/{tripId}")
+    public ResponseEntity<TripDTO> getTripById(@PathVariable Long tripId) {
+        return ResponseEntity.ok(tripService.getTripById(tripId));
+    }
+}
