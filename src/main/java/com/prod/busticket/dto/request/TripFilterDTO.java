c8435c11ca964bea4dbf9b3b9755954791cc5a50
Lungu Mihai <lungu2003mihai1@mail.ru>
2025-02-12 14:17:28 +0200

    Update the filter for the trip

--- src/main/java/com/prod/busticket/dto/request/TripFilterDTO.java
+++ src/main/java/com/prod/busticket/dto/request/TripFilterDTO.java
@@ -1,7 +1,15 @@
 package com.prod.busticket.dto.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class TripFilterDTO {
-    private String departureLocation;
-    private String arrivalLocation;
+    private String location;
     private java.time.LocalDateTime departureTime;
 }
