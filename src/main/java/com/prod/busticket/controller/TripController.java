ca13087dd78783e43a12360d487f542a229a0358
Lungu Mihai <lungu2003mihai1@mail.ru>
2025-02-12 14:16:58 +0200

    Add trip filtering endpoint

--- src/main/java/com/prod/busticket/controller/TripController.java
+++ src/main/java/com/prod/busticket/controller/TripController.java
@@ -1,11 +1,14 @@
 package com.prod.busticket.controller;
 
 import com.prod.busticket.dto.TripDTO;
+import com.prod.busticket.dto.request.TripFilterDTO;
 import com.prod.busticket.service.TripService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -18,5 +21,10 @@ public class TripController {
     @GetMapping("/{tripId}")
     public ResponseEntity<TripDTO> getTripById(@PathVariable Long tripId) {
         return ResponseEntity.ok(tripService.getTripById(tripId));
+    }
+
+    @PostMapping
+    public ResponseEntity<List<TripDTO>> getTripsByFilter(@RequestBody(required = false) TripFilterDTO filter) {
+        return ResponseEntity.ok(tripService.getTrips(filter));
     }
 }
